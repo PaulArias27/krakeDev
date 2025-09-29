721ea8237e1b15d684ed10d0ff4a1b87c76c9fd2
PaulArias27 <paularias2727@gmail.com>
2025-09-29 14:02:48 -0500

    el boton cambiar imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar = function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo = document.getElementById("imgSaludo");
+    cmpImagenSaludo.src = "./imagenes/crosty.gif";
     console.log ("enviar saludar");
 }
