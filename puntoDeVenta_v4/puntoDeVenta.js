83253121cec82f8f450fab5fcbc36711659831cf
PaulArias27 <paularias2727@gmail.com>
2025-10-02 14:22:53 -0500

    se invoco la funcion calcularIva en puntoDeVenta.js

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -48,6 +48,7 @@ calcularValorTotal = function () {
     mostrarTexto("lblDescuento",valorDescuento);
     //8. Invocar a calcularIVA y lo que devuelve guardar en la variable valorIVA
     // El IVA debe calcularse sobre el valor del subtotal menos el descuento
+    calcularIva = calcularIva(valorSubtotal - valorDescuento);
     //9. Mostrar el resultado en el componente lblValorIVA    
         /*
             Caso de prueba: 
@@ -64,6 +65,7 @@ calcularValorTotal = function () {
 
             Si el caso de prueba es exitoso, hacer un commit
         */
+    mostrarTexto("lblValorIVA",calcularIva);   
     //10. Invocar a calcularTotal y lo que devuelve guardar en la variable valorTotal
     //11. Mostrar el resultado en el componente lblTotal
     /*
