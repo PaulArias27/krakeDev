1fff0f7ba28341f9c6972ff09f3505f53d852591
PaulArias27 <paularias2727@gmail.com>
2025-10-06 13:44:08 -0500

    se inicio con la programacion del ejercicio promedio notas

---
+++ promedioNotas/promedioNotas.js
@@ -0,0 +1,20 @@
+calcularPromedioNotas = function(){
+    let cmpNota1;
+    let cmpNota2;
+    let cmpNota3;
+
+    cmpNota1 = document.getElementById("nota1");
+    let nota1 = cmpNota1.value;
+    let valor1Float = parseFloat(nota1);
+    cmpNota2 = document.getElementById("nota2");
+    let nota2 = cmpNota2.value;
+    let valor2Float = parseFloat(nota2);
+    cmpNota3 = document.getElementById("nota3");
+    let nota3 = cmpNota3.value;
+    let valor3Float = parseFloat(nota3);
+
+    let promedio = calcularPromedio(valor1Float,valor2Float,valor3Float);
+    let cmpDecimales= promedio.toFixed(2);
+    let mensaje = document.getElementById("lblPromedio");
+    mensaje.innerHTML = "tu promedio es: " + cmpDecimales; 
+}
---
+++ promedioNotas/serviciosPromedioNotas.js
@@ -0,0 +1,7 @@
+calcularPromedio = function(n1,n2,n3){
+    let cmpPromedio;
+    let promedio;
+    cmpPromedio = n1 + n2 + n3;
+    promedio = cmpPromedio / 3;
+    return promedio;
+}
