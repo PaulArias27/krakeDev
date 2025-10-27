df2d35959464f5c83593d16878ace5336f5df732
PaulArias27 <paularias2727@gmail.com>
2025-10-26 21:21:10 -0500

    se creo a clientes.js

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -34,3 +34,64 @@ crearProducto = function (){
         console.log("producto 2 tiene mayor stock");
     }
 }
+modificarAtributos = function(){
+    let cuenta = {
+        numero :"5323423423",
+        saldo : 0.0
+    }
+    cuenta.saldo = 100;
+    cuenta.saldo += 10;
+    console.log(cuenta.saldo);
+    
+}
+ 
+crearCliente = function(){
+    let cliente = {
+        cedula : "2000122727",
+        nombre : "juan"
+    };
+    let cliente1 = {};
+    cliente1.nombre = "paul";
+    cliente1.apellido = "arias";
+    cliente1.cedula = "2000122727";
+
+}
+
+probarIncrementoSaldo = function(){
+    let cuenta = {
+        numeroCuenta :"23456",
+        saldo : 34.0
+    }
+    incrementarSaldo(cuenta,100);
+    console.log(cuenta.saldo);
+}
+probarDeterminarMayor =function(){
+    let per1 = {
+        nombre : "daniel",
+        edad : 45
+    }
+    let per2 = {
+        nombre : "valeria",
+        edad: 48
+    }
+    let mayor = determinarMayor(per1,per2);
+    if (mayor != null){
+        console.log("el mayor es : " + mayor.nombre);
+
+    }
+}
+
+incrementarSaldo = function(cuenta,monto){
+    cuenta.saldo += monto;
+}
+
+determinarMayor = function(persona1,persona2){
+    if(persona1.edad > persona2.edad ){
+        return persona1;
+    }else if(persona2.edad > persona1.edad){
+        return persona2;
+    }else{
+        return null;
+    }
+
+}
