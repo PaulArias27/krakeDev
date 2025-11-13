efe21c1299b97f21b3aa89b998e2b77d38905611
PaulArias27 <paularias2727@gmail.com>
2025-11-12 22:38:51 -0500

    se crearon las funciones de transacciones.js

--- TrabajoGrupal2/transacciones.js
+++ TrabajoGrupal2/transacciones.js
@@ -15,6 +15,17 @@ cargar=function(){
     si existe retorna el objeto cuenta, caso contrario retorna null. 
 */
 buscarCuenta=function(numeroCuenta){
+    let elementoCuenta;
+    let cuentaEncontrada = null;
+    for(let i=0;i < cuentas.length; i++){
+        elementoCuenta = cuentas[i];
+        if(elementoCuenta.numeroCuenta == numeroCuenta){
+            cuentaEncontrada = elementoCuenta;
+        }else{
+            return cuentaEncontrada;
+        }  
+    }
+  
 
 }
 
@@ -22,13 +33,29 @@ ejecutarBusqueda=function(){
     //toma el numero de cuenta de la caja de texto
     //invoca a buscarCuenta y guarda el resultado en una variable
     //Si el resultado es diferente de null, muestra en pantalla, caso contrario muestra un alert
+    let cmpNumCuenta = recuperarTexto("caja1");
+    let busquedaCuenta = buscarCuenta(cmpNumCuenta);
+    let pantalla;
+    if(busquedaCuenta != null){
+        pantalla = mostrarTexto("busqueda",busquedaCuenta);
+        return pantalla;
+    }else{
+        alert("LA CUENTA NO EXISTE");
+    }
 }
 
 depositar=function(numeroCuenta,monto){
     let cuentaAfectada;
     //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
     //Al saldo actual de la cuenta afectada, le suma el monto que recibe como parámetro
-}
+    cuentaAfectada = buscarCuenta(numeroCuenta);
+    if(cuentaAfectada != null){
+        cuentaAfectada.saldo = cuentaAfectada.saldo + monto;
+        return cuentaAfectada;
+    }else{
+        return null;
+    } 
+} 
 
 ejecutarDeposito=function(){
     //Toma el numero de cuenta ingresado en la caja de texto
@@ -36,6 +63,14 @@ ejecutarDeposito=function(){
     //invoca a depositar
     //Muestra un mensaje TRANSACCION EXITOSA
     //Muestra en pantalla el nuevo saldo de la cuenta
+    let cmpNumCuenta = recuperarTexto("caja1");
+    let cmpMonto = recuperarInt("caja2");
+    let cmpDeposito = depositar(cmpNumCuenta,cmpMonto);
+    if (cmpDeposito != null){
+        alert("TRANSACCION EXITOSA")
+        mostrarTexto("saldo","SALDO: "+cmpDeposito.saldo);
+    }
+
 }
 
 depositar=function(numeroCuenta,monto){
@@ -51,4 +86,15 @@ retirar=function(numeroCuenta,monto){
     //Si el saldo es suficiente,al saldo actual de la cuenta afectada, le resta el monto que recibe como parámetro
     //Si el saldo no es suficiente, muestra un alert SALDO INSUFICIENTE
     //Si logra retirar muestra un mensaje TRANSACCION EXITOSA y muestra en pantalla el nuevo saldo de la cuenta
+    cuentaAfectada = buscarCuenta(numeroCuenta);
+    if(cuentaAfectada != null){
+        if(cuentaAfectada.saldo >= monto){
+            cuentaAfectada.saldo = cuentaAfectada.saldo - monto;
+        }else{
+            alert("SALDO INSUFICIENTE");
+            
+        }
+    }else{
+        alert("LA CUENTA NO EXISTE");
+    }
 }
